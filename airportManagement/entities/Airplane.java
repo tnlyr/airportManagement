f2d34b4447688ae65e9640816f56a16cfbd87d42
L <gaming0565@gmail.com>
2022-04-23 18:50:19 -0400

    Commit

--- airportManagement/entities/Airplane.java
+++ airportManagement/entities/Airplane.java
@@ -1,4 +1,88 @@
 package entities;
 
-public class Airplane {
+public abstract class Airplane {
+    private String model, id;
+    private int capacity, range, speed;
+    private double emptyWeight, maxWeight;
+
+    public Airplane() {}
+
+    public Airplane(String model, String id, int capacity, int range, int speed, double emptyWeight, double maxWeight) {
+        this.model = model;
+        this.id = id;
+        this.capacity = capacity;
+        this.range = range;
+        this.speed = speed;
+        this.emptyWeight = emptyWeight;
+        this.maxWeight = maxWeight;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public double getEmptyWeight() {
+        return emptyWeight;
+    }
+
+    public void setEmptyWeight(double emptyWeight) {
+        this.emptyWeight = emptyWeight;
+    }
+
+    public double getMaxWeight() {
+        return maxWeight;
+    }
+
+    public void setMaxWeight(double maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    @Override
+    public String toString() {
+        return "Airplane{" +
+                "model='" + model + '\'' +
+                ", id='" + id + '\'' +
+                ", capacity=" + capacity +
+                ", range=" + range +
+                ", speed=" + speed +
+                ", emptyWeight=" + emptyWeight +
+                ", maxWeight=" + maxWeight +
+                '}';
+    }
 }
--- airportManagement/management/InputValidator.java
+++ airportManagement/management/InputValidator.java
@@ -1,4 +1,43 @@
 package management;
 
+import java.util.Scanner;
+
 public class InputValidator {
+
+    public int inputInt(Scanner s){
+        int number;
+        while (true){
+            try{
+                number = Integer.parseInt(s.nextLine());
+                return number;
+            } catch (Exception e) {
+                System.out.println("Invalid input! Please try again: ");
+            }
+        }
+    }
+
+    public double inputDouble(Scanner s){
+        double number;
+        while (true) {
+            try {
+                number = Double.parseDouble(s.nextLine());
+            } catch (Exception e) {
+                System.out.println("Invalid input! Please try again: ");
+                ;
+            }
+        }
+    }
+
+    public String inputString(Scanner s){
+        String input;
+        while (true) {
+            input = s.nextLine();
+            if (input.length() > 50) {
+                System.out.println("The maximum length of the input is 50 characters! Please try again: ");
+                continue;
+            }
+            return input;
+        }
+    }
+
 }
