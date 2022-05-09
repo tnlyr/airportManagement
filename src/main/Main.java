8c911b75ce1c6b91f8df368ace41e8e3b83335dc
L <gaming0565@gmail.com>
2022-05-09 18:13:40 -0400

    fix going back to main menu

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,24 +7,28 @@ import objects.Airport;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
+    static ArrayList<Airport> listAirports = new ArrayList<>();
+    static ArrayList<Airplane> listAirplanes = new ArrayList<>();
     public static void main(String[] args) {
 	// write your code here
         InputManagement inputManagement = new InputManagement();
         Scanner sc = new Scanner(System.in);
 
-        ArrayList<Airport> listAirports = new ArrayList<>();
-        ArrayList<Airplane> listAirplanes = new ArrayList<>();
+        //ArrayList<Airport> listAirports = new ArrayList<>();
+        //ArrayList<Airplane> listAirplanes = new ArrayList<>();
 
-        System.out.println("----------= Main Menu =----------");
-        System.out.println("1. Create new objects");
-        System.out.println("2. Manage airports");
-        System.out.println("3. Manage civilian airplanes");
-        System.out.println("4. Manage helicopters");
-        System.out.println("Exit");
-        System.out.println("----------------------------------");
-        System.out.print("Enter your choice: ");
-        int choice = Integer.parseInt(sc.nextLine());
         while (true) {
+
+            System.out.println("----------= Main Menu =----------");
+            System.out.println("1. Create new objects");
+            System.out.println("2. Manage airports");
+            System.out.println("3. Manage civilian airplanes");
+            System.out.println("4. Manage helicopters");
+            System.out.println("Exit");
+            System.out.println("----------------------------------");
+            System.out.print("Enter your choice: ");
+            int choice = Integer.parseInt(sc.nextLine());
+
             switch (choice) {
                 case 1:
                     inputManagement.inputData(sc, listAirports, listAirplanes);
@@ -37,11 +41,6 @@ public class Main {
                     break;
                 case 4:
                     inputManagement.heliManagementInput(sc, listAirplanes, listAirports);
-                    break;
-                case 5:
-                    //System.out.println(listAirports);
-                    //System.out.println(listAirplanes);
-                    //main(args);
                     break;
                 default:
                     System.out.println("Exiting...");
